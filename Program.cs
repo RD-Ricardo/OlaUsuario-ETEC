0eaa1046b21bdb3531e92027a436f606f0f6258b
RD-Ricardo <ricardodias169@gmail.com>
2021-03-06 17:53:34 -0300

    finalizando Programa ola usuario

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string nome;
+            Console.WriteLine("Digite seu nome");
+            Console.WriteLine("----------------");
+            nome = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"\n Olá {nome}");
+        }
+    }
+}
